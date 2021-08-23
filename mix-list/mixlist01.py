925dddd963f880b79c18ca896907e00caf3b3a39
plattanzi <plattanzi@users.noreply.github.com>
2021-08-23 21:14:20 +0000

    mixed lists

---
+++ mix-list/mixlist01.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+my_list = [ "192.168.0.5", 5060, "UP" ]
+#pulling the first item from the list
+print("The first item in the list (IP): " + my_list[0] )
+print("The second item in the list (port): " + str(my_list[1]) )
+#changing integer to a string
+# display only the IP addresses to the screen.
+iplist = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
+
+# example 1 - add up the strings
+print("IP addresses: " + iplist[3] + ", and " + iplist[4])
+
+# example 2 - use the comma separator 
+print("IP addresses:", iplist[3], ", and", iplist[4])
+
+# example 3 - use an 'f-string'
+print(f"IP addresses: {iplist[3]}, and {iplist[4]}")
+
